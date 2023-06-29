1db43694d0c48572b8feb8a5b8b0dc58b466247e
srikredd <kundurusrikanthreddy9@gmail.com>
2023-06-29 05:57:13 +0530

    test

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "{region}"  # Replace with your desired AWS region
 }
 
-resource "aws_instance" "terraform_sample" {
+resource "aws_instance" "test" {
   ami           = "{ami_id}"  # Replace with your desired AMI ID
   instance_type = "t2.micro"
 }
