749d03962ab51a01a496fb8ceaecda4e9f5e4aa1
srikredd <kundurusrikanthreddy9@gmail.com>
2023-06-29 05:45:57 +0530

    sample terrafrom cici demo

---
+++ main.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  region = "{region}"  # Replace with your desired AWS region
+}
+
+resource "aws_instance" "terraform_sample" {
+  ami           = "{ami_id}"  # Replace with your desired AMI ID
+  instance_type = "t2.micro"
+}
